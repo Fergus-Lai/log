6b058cae8212bd1f75decd11d2c497e67cefff35
Fergus Lai <ferguslai724@gmail.com>
2022-03-18 16:23:29 +0000

    Added A Home Screen

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,10 +5,15 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import Home from '../src/components/screens/Home';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+test('renders app correctly', () => {
   renderer.create(<App />);
+});
+
+test('render home correctly', () => {
+  renderer.create(<Home />);
 });
